c9feb2889c6f3745bb1a37f9abdc376579ae41d2
lailaebrahim <ebrahimlaila815@gmail.com>
2024-02-25 03:38:07 +0200

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,5 @@
 #Using Puppet, install flask from pip3.
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => 'pip3 install flask==2.1.0',
-  require => Package['python3-pip'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 #Using Puppet, create a manifest that kills a process named killmenow.
-exec { 'kill_killmenow':
+exec { 'pkill':
   command => 'pkill killmenow',
-  onlyif  => 'pgrep killmenow',
+  provide => 'shell'
 }
