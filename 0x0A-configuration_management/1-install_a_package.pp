8e7e97185624363d97a4666cae98bccde1a27882
lailaebrahim <ebrahimlaila815@gmail.com>
2024-02-26 17:35:01 +0200

    Task1 Fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
 #Using Puppet, install flask from pip3.
-package { 'flask' :
-    ensure   => '2.0.1',
-    provider => 'pip3',
+package { 'flask':
+  ensure   => '2.0.1',
+  provider => 'pip3',
 }
 
-package { 'Werkzeug' :
-    ensure   => '2.1.1',
-    provider => 'pip3'
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
