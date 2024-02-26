2b450b2729368188bbbd1b7761f200b221255814
lailaebrahim <ebrahimlaila815@gmail.com>
2024-02-26 15:50:57 +0200

    Fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 #Using Puppet, install flask from pip3.
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'install_flask':
+  command     => 'pip3 install flask==2.1.0',
+  creates     => '/usr/local/lib/python3.x/site-packages/flask/__init__.py', # Adjust the path based on your Python installation
+  refreshonly => true,
 }
