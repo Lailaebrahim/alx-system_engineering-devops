b2280c700cc97a9d1aea0dfeb9e0edef4da593f8
lailaebrahim <ebrahimlaila815@gmail.com>
2024-02-26 15:55:03 +0200

    Fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 #Using Puppet, install flask from pip3.
-exec { 'install_flask':
-  command     => 'pip3 install flask==2.1.0',
-  creates     => '/usr/local/lib/python3.x/site-packages/flask/__init__.py', # Adjust the path based on your Python installation
-  refreshonly => true,
-}
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 #Using Puppet, create a manifest that kills a process named killmenow.
-exec { 'pkill':
-  command => 'pkill killmenow',
-  provider => 'shell'
-}
+exec { 'pkill killmenow' :
+    path    => '/bin/',
+    command => 'pkill killmenow',
+    }
