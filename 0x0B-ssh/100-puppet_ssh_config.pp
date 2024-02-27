f3c8580b71f0c6e2be35c7124669f909489dd5d4
Lailaebrahim <lailaebrahim815@gmail.com>
2024-02-26 16:52:29 -0800

    Fix regex

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
 #set up your client SSH configuration file
-file_line { 'private key'
-    path  => '~/etc/ssh/ssh_config',
-    match => 'IdentityFile.*'
-    line  => 'IdentityFile ~/.ssh/school',
-    }
-file_line { 'password'
-    path  => '~/etc/ssh/ssh_config',
-    match => 'PasswordAuthentication.*',
-    line  => 'PasswordAuthentication no',
+file_line { 'private key':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    IdentityFile ~/.ssh/school',
+  match => '^#?\s*IdentityFile.*',
+}
+
+file_line { 'password':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    PasswordAuthentication no',
+  match => '^#?\s*PasswordAuthentication.*',
 }
