e99df8348998d64427eb82a65b6078192ce3b340
Lailaebrahim <ebrahimlaila815@gmail.com>
2024-02-28 14:39:21 -0800

    fix puppet-lint

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,29 +1,29 @@
 #update system
 exec { 'update system':
-        command => '/usr/bin/apt-get update',
+  command => '/usr/bin/apt-get update',
 }
-	
+
 #install nginx web server
 package { 'nginx' :
-	ensure  => installed,
-	require => Exec['update system']
+  ensure  => installed,
+  require => Exec['update system']
 }
 
 #start nginx service
 service { 'nginx' :
-	ensure  => running,
-	enable  => true,
-	refresh => true,
-	require => Package['nginx']
+  ensure  => running,
+  enable  => true,
+  refresh => true,
+  require => Package['nginx']
 }
 
 #write Hello World! in the root page
 file {'/var/www/html/index.nginx-debian.html':
-     content => 'Hello World!'
+  content => 'Hello World!'
 }
 
 #configure nginx server to redirect page redirect_me to another
 exec {'redirect_me':
-     command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-     provider => 'shell'
+  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell'
 }
