49bb262da399442615f7c1b8564adbd1e9d8d3a3
Lailaebrahim <lailaebrahim815@gmail.com>
2024-02-28 14:26:08 -0800

    Do it by another way

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,40 +1,29 @@
+#update system
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+	
 #install nginx web server
 package { 'nginx' :
-	ensure => installed,
+	ensure  => installed,
+	require => Exec['update system']
 }
 
 #start nginx service
 service { 'nginx' :
-	ensure => running,
-	enable => true,
+	ensure  => running,
+	enable  => true,
+	refresh => true,
+	require => Package['nginx']
 }
 
-
-#Configure your Nginx server so that /redirect_me is redirecting to another page.
-file { '/etc/nginx/sites-available/default':
-     ensure  => file,
-     content => "server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;  
-	root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-
-        location / {
-                # First attempt to serve request as file, then
-                # as directory, then fall back to displaying a 404.
-                try_files $uri $uri/ =404;
-        }  root /var/www/html;
-
-}"
-	notify => Service['nginx'],
-}
-
-#Nginx at its root return a page that contains the string Hello World!
+#write Hello World! in the root page
 file {'/var/www/html/index.nginx-debian.html':
-     ensure => file,
-     content => 'Hello World!"
-     notify => Service['nginx'],
+     content => 'Hello World!'
+}
+
+#configure nginx server to redirect page redirect_me to another
+exec {'redirect_me':
+     command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+     provider => 'shell'
 }
