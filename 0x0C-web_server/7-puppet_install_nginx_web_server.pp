815bfac491376fe6355e1564e86539085f4dfc00
Lailaebrahim <ebrahimlaila815@gmail.com>
2024-02-28 14:49:19 -0800

    Fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -23,6 +23,6 @@ file {'/var/www/html/index.nginx-debian.html':
 
 #configure nginx server to redirect page redirect_me to another
 exec {'redirect_me':
-  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  command  => 'sudo sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
   provider => 'shell'
 }
