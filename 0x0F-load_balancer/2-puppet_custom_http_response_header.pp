5b8b4b6b1434cddf609229d79504b25f42947520
Lailaebrahim <ebrahimlaila815@gmail.com>
2024-03-05 13:20:31 -0800

    Fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -18,19 +18,19 @@ service { 'nginx' :
 #write Hello World! in the root page
 file {'/var/www/html/index.nginx-debian.html':
   content => 'Hello World!',
-  notify  => Service['nginx'],
+  notify  => Service['nginx']
     }
 
 #configure nginx server to redirect page redirect_me to another
 exec {'redirect_me':
   command  => 'sudo sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
   provider => 'shell',
-  notify   => Service['nginx'],
+  notify   => Service['nginx']
       }
       
 #HTTP header named X-Served-By it's value the hostname of the server Nginx is running on
 exec {'HTTP header':
-   command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+   command  => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
    provider => 'shell',
-   notify   => Service['nginx'],
+   notify   => Service['nginx']
      }
