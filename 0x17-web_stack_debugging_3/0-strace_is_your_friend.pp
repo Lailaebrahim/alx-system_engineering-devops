946497eccd1b2a9c3497bbfdcbabb8a4f3903f52
Lailaebrahim <ebrahimlaila815@gmail.com>
2024-06-05 05:08:33 -0700

    Fix

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#Solve Bug by fixing the spelling of phpp extension to php
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
