3b8ed2f011b02845a9f96973161a481b115fe047
lailaebrahim <ebrahimlaila815@gmail.com>
2024-06-05 03:19:33 +0300

    Task 0, 1, 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,6 +3,18 @@
   (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given,
  the function should return 0.
 """
+
+import requests
+
+
 def number_of_subscribers(subreddit):
     """Return number of subscribers"""
-    
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    header = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=header, allow_redirects=False )
+    if response.status_code == 200:
+        subs_data = response.json()
+        subs_num = subs_data['data']['subscribers']
+        return subs_num
+    else:
+        return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3#!/usr/bin/python3
+""" a function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """first 10 hot posts listed for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
+    header = {"User-Agent": "Mozilla/5.0"}
+    response_url = requests.get(url, headers=header, allow_redirects=False).json()
+    try:
+        for post in response_url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
